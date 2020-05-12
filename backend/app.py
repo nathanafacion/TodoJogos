bb394990c40d7987b947765416649d894b24fcc3
nathanafacion <nathanafacion@gmail.com>
2020-05-12 16:03:07 -0300

    Adição do banco de dados tinyDB

--- backend/app.py
+++ backend/app.py
@@ -8,25 +8,26 @@ DEBUG = True
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
 db = TinyDB('db.json')
+#JOGOS = db.all()
 
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-JOGOS = db.all()
 
 @app.route('/jogos', methods=['GET', 'POST'])
 def all_jogos():
     response_object = {'status': 'success'}
     if request.method == 'POST':
         post_data = request.get_json()
-        JOGOS.insert({
+        db.insert({
              'id': uuid.uuid4().hex,
             'title': post_data.get('title'),
             'type': post_data.get('type'),
         })
         response_object['message'] = 'Jogo adicionado'
     else:
-        response_object['jogos'] = JOGOS
+        response_object['jogos'] = db.all()
     return jsonify(response_object)
 
 @app.route('/jogos/<jogo_id>', methods=['GET', 'PUT', 'DELETE'])
@@ -34,14 +35,14 @@ def single_jogo(jogo_id):
     response_object = {'status': 'success'}
     if request.method == 'GET':
         return_jogo = ''
-        for jogo in JOGOS:
+        for jogo in db.all():
             if jogo['id'] == jogo_id:
                 return_jogo = jogo
         response_object['jogo'] = return_jogo
     if request.method == 'PUT':
         post_data = request.get_json()
         remove_jogo(jogo_id)
-        JOGOS.insert({
+        db.insert({
             'id': uuid.uuid4().hex,
             'title': post_data.get('title'),
             'type': post_data.get('type'),
@@ -54,11 +55,8 @@ def single_jogo(jogo_id):
     return jsonify(response_object)
 
 def remove_jogo(jogo_id):
-    for jogo in JOGOS:
-        if jogo['id'] == jogo_id:
-            JOGOS.remove(jogo)
-            return True
-    return False
+    r = Query()
+    db.remove(r.id == jogo_id)
 
 if __name__ == '__main__':
     app.run()
